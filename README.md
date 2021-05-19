0a5bf590ed18d32cfb824b170cb170793aead706
Eko SW <swdev.bali@gmail.com>
2021-05-19 11:41:07 +0700

    works. flawlessly

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # tailwind-postcss-gulp-browsersync
+https://dev.to/salttechno/how-to-setup-tailwind-css-with-postcss-browsersync-1j41
+
+bootcamp onsite online serentak:
+- champion: selesai, +popular => badge
+- spt dev.to
---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+// gulpfile.js
+const { watch, series, src, dest } = require("gulp");
+var browserSync = require("browser-sync").create();
+var postcss = require("gulp-postcss");
+const imagemin = require("gulp-imagemin");
+
+// Task for compiling our CSS files using PostCSS
+function cssTask(cb) {
+    return src("./src/*.css") // read .css files from ./src/ folder
+        .pipe(postcss()) // compile using postcss
+        .pipe(dest("./dist/css")) // paste them in ./dist/css folder
+        .pipe(browserSync.stream());
+    cb();
+}
+
+// Task for minifying images
+function imageminTask(cb) {
+    return src("./dist/images/*")
+        .pipe(imagemin())
+        .pipe(dest("./dist/images"));
+    cb();
+}
+
+// Serve from browserSync server
+function browsersyncServe(cb) {
+    browserSync.init({
+        server: {
+            baseDir: "./",
+        },
+    });
+    cb();
+}
+
+function browsersyncReload(cb) {
+    browserSync.reload();
+    cb();
+}
+
+// Watch Files & Reload browser after tasks
+function watchTask() {
+    watch("./**/*.html", browsersyncReload);
+    watch(["./src/*.css"], series(cssTask, browsersyncReload));
+}
+
+// Default Gulp Task
+exports.default = series(cssTask, browsersyncServe, watchTask);
+exports.css = cssTask;
+exports.images = imageminTask;
